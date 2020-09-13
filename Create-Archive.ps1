26657fe35be49478ea892890c86f6396cd014f48
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-09-13 11:24:53 +0900

    未処理例外発生時にバックトレースを表示するデバッグ機能を追加。

--- Create-Archive.ps1
+++ Create-Archive.ps1
@@ -1,5 +1,12 @@
-$version = "0.1"
+Param([switch] $Debug)
+
+$version = "0.2.0"
+
 $dir = "VoiAlarm/bin/Release"
+if ($Debug)
+{
+    $dir = "VoiAlarm/bin/Debug"
+}
 
 $compressArgs = @{
     Path = "README.md", "LICENSE", "$dir/*.exe", "$dir/*.exe.config", "$dir/Settings.default.xml", "$dir/MaterialDesignColors.dll", "$dir/MaterialDesignThemes.Wpf.dll", "$dir/NAudio.dll"
--- VoiAlarm/App.xaml.cs
+++ VoiAlarm/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace VoiAlarm
 {
@@ -19,6 +20,8 @@ namespace VoiAlarm
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+
             if (!mutex.WaitOne(0, false))
             {
                 MessageBox.Show($"{appName} はすでに起動しています。", appName, MessageBoxButton.OK);
@@ -36,6 +39,14 @@ namespace VoiAlarm
                 mutex.Close();
                 mutex = null;
             }
+
+            DispatcherUnhandledException -= OnDispatcherUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var message = e.Exception.Message + Environment.NewLine + e.Exception.StackTrace;
+            MessageBox.Show(message, $"{appName} 未処理例外");
         }
     }
 }
